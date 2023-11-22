b4c7744be0b1ad9f5b26c5436a3dc8fb98a1e545
stephenhau <stephen.hau@binus.ac.id>
2023-11-22 17:55:17 +0700

    pool

--- Models/PoolBranch.cs
+++ Models/PoolBranch.cs
@@ -8,9 +8,6 @@ namespace JSE.Models
         [Key]
         public string pool_city { get; set; }
 
-        [MaxLength(255)]
-        public string pool_address { get; set; }
-
         [StringLength(13, MinimumLength = 10, ErrorMessage = "Must be between 10 and 13 characters long.")]
         public string pool_phone { get; set; }
         public ICollection<Delivery> SendingDeliveries { get; set; }    
